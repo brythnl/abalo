3d37161051dc1013bfdb258d047a4089f424e396
Bryan <bryanathanaelj@gmail.com>
2023-03-22 15:51:00 +0100

    Add migration table for article_has_categories

---
+++ database/migrations/2023_03_22_142622_create_ab_article_has_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ab_article_has_categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedTinyInteger('ab_articlecategory_id')->nullable(false)
+                ->comment('Referenz auf eine Artikelkategorie');
+            $table->unsignedTinyInteger('ab_article_id')->nullable(false)
+                ->comment('Referenz auf einen Artikel');
+
+            $table->foreign('ab_articlecategory_id')->references('id')->on('ab_articlecategories');
+            $table->foreign('ab_article_id')->references('id')->on('ab_articles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ab_article_has_categories');
+    }
+};
