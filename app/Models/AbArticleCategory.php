ebd85dfecfd317ff82c5abf37dd7a44fc0e32ac3
Bryan <bryanathanaelj@gmail.com>
2023-03-27 14:27:59 +0200

    Create model for ab_articlecategories table

---
+++ app/Models/AbArticleCategory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AbArticleCategory extends Model
+{
+    use HasFactory;
+
+    protected $table = 'ab_articlecategories';
+
+    public $timestamps = false;
+
+    protected $guarded = [];
+}
