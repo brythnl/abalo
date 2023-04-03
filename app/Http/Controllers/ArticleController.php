8d86a38e066c7cc691ec8a86720eaf22220e8e67
Bryan <bryanathanaelj@gmail.com>
2023-04-03 11:34:12 +0200

    Add pgsql escape string

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,7 +6,7 @@ use App\Models\AbArticle;
 class ArticleController extends Controller
 {
     public function getProductList(){
-        $filter = isset($_GET['search'])?$_GET['search']:'';
+        $filter = isset($_GET['search'])?pg_escape_string($_GET['search']):'';
         $result = AbArticle::query()->where(('ab_name'),'ILIKE','%'.strtolower($filter).'%')->get()->toArray();
         return view('/articles',['result'=>$result]);
     }
