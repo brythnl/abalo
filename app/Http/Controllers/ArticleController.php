6f04c161973d38b7b12987b208f1341531cdf608
Bryan <bryanathanaelj@gmail.com>
2023-04-04 16:07:47 +0200

    Move articles view into views/pages

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,6 +8,6 @@ class ArticleController extends Controller
     public function getProductList(){
         $filter = isset($_GET['search'])?pg_escape_string($_GET['search']):'';
         $result = AbArticle::query()->where(('ab_name'),'ILIKE','%'.strtolower($filter).'%')->get()->toArray();
-        return view('/articles',['result'=>$result]);
+        return view('pages/articles',['result'=>$result]);
     }
 }
