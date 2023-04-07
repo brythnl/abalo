d90127d1ae6bbabd907d8461a81c53b382a81472
Bryan <bryanathanaelj@gmail.com>
2023-04-07 20:45:39 +0200

    Add page title

--- resources/views/new-article.blade.php
+++ resources/views/new-article.blade.php
@@ -1,6 +1,9 @@
 <x-layout>
+    <x-slot:title>New Article</x-slot>
+
     <div id="article-form-container">
 
     </div>
+
     <script src="{{ asset('/js/newarticle.js') }}"></script>
 </x-layout>
