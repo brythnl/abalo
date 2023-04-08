94ef40504ec8cfaff5cfb194346ef79bb9ef10da
Bryan <bryanathanaelj@gmail.com>
2023-04-08 02:04:55 +0200

    Add article saved success message

--- resources/views/new-article.blade.php
+++ resources/views/new-article.blade.php
@@ -1,6 +1,10 @@
 <x-layout>
     <x-slot:title>New Article</x-slot>
 
+    @if (session('successMessage'))
+        <p>{{ session('successMessage') }}</p>
+    @endif
+
     <div id="article-form-container">
 
     </div>
