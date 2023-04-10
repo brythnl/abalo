fca63579235d031b610e3e8296e29bf61964bb01
Bryan <bryanathanaelj@gmail.com>
2023-04-10 18:20:31 +0200

    Set buttons to set cookies and toggle banner

--- public/js/cookiecheck.js
+++ public/js/cookiecheck.js
@@ -14,4 +14,24 @@ function getCookie(name) {
 }
 
 
+/* ------------------------ App-specific ------------------------ */
+// DOM Elements
+const acceptCookieButton = document.getElementById("accept-cookie-button");
+const refuseCookieButton = document.getElementById("refuse-cookie-button");
+const cookieBanner = document.getElementById("cookie-banner");
 
+function acceptAbaloCookie() {
+    deleteCookie("abalo_cookie");
+    setCookie("abalo_cookie", "true");
+    cookieBanner.style.display = "none";
+}
+
+acceptCookieButton.addEventListener("click", acceptAbaloCookie);
+refuseCookieButton.addEventListener("click", () => cookieBanner.style.display = "none");
+
+let cookie = getCookie("abalo_cookie");
+if (cookie.includes("true")) {
+    cookieBanner.style.display = "none";
+} else {
+    cookieBanner.style.display = "block";
+}
