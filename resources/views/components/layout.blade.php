beee3a311799fba346111bcfb3bf08af26403d66
Bryan <bryanathanaelj@gmail.com>
2023-04-10 18:15:52 +0200

    Include cookie permission logic

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="{{ asset('/js/cookiecheck.js') }}" defer></script>
 
     <title>{{ $title ?? 'Abalo' }}</title>
 </head>
