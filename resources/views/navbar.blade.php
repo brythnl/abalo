99f45709789ef6b8f43862e67b57d4f32d432b60
Bryan <bryanathanaelj@gmail.com>
2023-04-15 16:16:38 +0200

    Move navbar as a component

--- resources/views/navbar.blade.php
+++
@@ -1,8 +0,0 @@
-<x-layout>
-    @section('navbar')
-    <div>
-    <ul class ="navbar" id="navBar"></ul>
-    </div>
-    <script src="{{ asset('/js/navbar.js') }}"></script>
-    @endsection
-</x-layout>
