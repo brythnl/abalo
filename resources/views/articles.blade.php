e94aec9a951e4b41a6e9b9087e27ef44de71fd3a
Bryan <bryanathanaelj@gmail.com>
2023-04-15 16:12:55 +0200

    Reformat code

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -4,33 +4,36 @@
         <x-navbar></x-navbar>
 
         <main>
-        <form method="GET" action="/articles">
-            <label for="search">search article name :</label>
-            <input type="search" name="search" id="search">
-            <input type="submit" value="submit">
-        </form>
+            <form method="GET" action="/articles">
+                <label for="search">search article name :</label>
+                <input type="search" name="search" id="search">
+                <input type="submit" value="submit">
+            </form>
 
-        <x-cart-overview></x-cart-overview>
-        <table>
-            <tbody>
-            <tr>
-                <th>images</th>
-                <th>name</th>
-                <th>price</th>
-                <th>description</th>
-            </tr>
-            @foreach($result as $table)
-                <tr>
-                    <td><img alt="{{$table['ab_name']}}" src={{$dir[$table['id']]}}></td>
-                    <td class="article-name">{{$table['ab_name']}}</td>
-                    <td class="article-price">{{$table['ab_price']}}</td>
-                    <td class="article-desc">{{$table['ab_description']}}</td>
-                    <td><a href=# class="add-to-cart-button" style="text-decoration: none; color: black;">+</a></td>
-                    <td><a href=# class="remove-from-cart-button" style="text-decoration: none; color: black;">-</a>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
+            <x-cart-overview></x-cart-overview>
+
+            <table>
+                <tbody>
+                    <tr>
+                        <th>images</th>
+                        <th>name</th>
+                        <th>price</th>
+                        <th>description</th>
+                    </tr>
+                    @foreach ($result as $table)
+                        <tr>
+                            <td><img alt="{{ $table['ab_name'] }}" src={{ $dir[$table['id']] }}></td>
+                            <td class="article-name">{{ $table['ab_name'] }}</td>
+                            <td class="article-price">{{ $table['ab_price'] }}</td>
+                            <td class="article-desc">{{ $table['ab_description'] }}</td>
+                            <td><a href=# class="add-to-cart-button" style="text-decoration: none; color: black;">+</a>
+                            </td>
+                            <td><a href=# class="remove-from-cart-button"
+                                    style="text-decoration: none; color: black;">-</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </main>
 </x-layout>
