9821890d352eb79c5e8ca786bd4166d40922e4e0
Bryan <bryanathanaelj@gmail.com>
2023-05-11 22:02:56 +0200

    Add API routes for cart management: POST: Add to cart DELETE: Remove from cart GET: Get cart items

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ShoppingcartController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/shoppingcart/{shoppingcartid}/{articleid}', [ShoppingcartController::class, 'addToCart_api']);
+Route::delete('/shoppingcart/{shoppingcartid}/articles/{articleid}', [ShoppingcartController::class, 'removeFromCart_api']);
+Route::get('/shoppingcart/items/{shoppingcartid}', [ShoppingcartController::class, 'getCartItems_api']);
