637bf0b29c75f786cc0917dc43ded4fc9201b513
Bryan <bryanathanaelj@gmail.com>
2023-05-30 16:05:10 +0200

    Add nav bar component

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,6 @@
 <x-layout>
     <div id="app">
+        <nav-bar></nav-bar>
         <site-header></site-header>
         <site-body :show-impressum="showImpressum"></site-body>
         <site-footer @toggle-impressum="toggleImpressum"></site-footer>
