286404d80f935fbe7fffec69c6478b446759276d
Bryan <bryanathanaelj@gmail.com>
2023-06-05 10:19:25 +0200

    Remove ArticleSearch component (integrated to SiteBody)

--- public/js/vue/app.js
+++ public/js/vue/app.js
@@ -2,11 +2,9 @@ import NavBar from './navbar.js';
 import SiteHeader from './siteheader.js';
 import SiteBody from './sitebody.js';
 import SiteFooter from './sitefooter.js';
-import ArticleSearch from './articlesearch.js';
 
 let vm = Vue.createApp({
     components: {
-        ArticleSearch,
         NavBar,
         SiteHeader,
         SiteBody,
