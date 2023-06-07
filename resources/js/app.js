b1475e06f128b0bab452d4b781c1c8267f6ae3b8
Bryan <bryanathanaelj@gmail.com>
2023-06-08 00:13:10 +0200

    Integrate Vue instance into app.js in resources

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
 import './bootstrap';
+import { createApp } from 'vue';
+
+import NavBar from './navbar.js';
+import SiteHeader from './siteheader.js';
+import SiteBody from './sitebody.js';
+import SiteFooter from './sitefooter.js';
+
+Vue.createApp({
+    components: {
+        NavBar,
+        SiteHeader,
+        SiteBody,
+        SiteFooter
+    },
+    data() {
+        return {
+            showImpressum: false,
+        };
+    },
+    methods: {
+        toggleImpressum() {
+            this.showImpressum = !this.showImpressum;
+        }
+    },
+}).mount('#app');
