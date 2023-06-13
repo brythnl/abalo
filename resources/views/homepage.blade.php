81a931e550762fa76c2e5c6ac4f2fc2ea85dc7e2
Bryan <bryanathanaelj@gmail.com>
2023-06-14 00:31:27 +0200

    Add vue-scroll-up component

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,6 +4,14 @@
         <site-header></site-header>
         <site-body :show-impressum="showImpressum"></site-body>
         <site-footer @toggle-impressum="toggleImpressum"></site-footer>
+        <vue-scroll-up
+          tag="div"
+          custom-class="vue-scroll-up"
+          :scroll-duration="1000"
+          :scroll-y="250"
+          :always-show="true">
+        ^
+        </vue-scroll-up>
     </div>
 
     @vite('resources/js/app.js')
