85c70a95bb61a94b4fdd6c6545adb8ea49b48347
Bryan <bryanathanaelj@gmail.com>
2023-06-16 20:54:45 +0200

    Rename nachricht websocket connection

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,9 +9,8 @@ import SiteFooter from './sitefooter.js';
 import NewArticle from './sitenewarticle.js';
 import MyArticle from './myarticle.js';
 
-let conn = new WebSocket('ws://localhost:8085/nachricht');
-
-conn.onmessage = function(e) {
+let nachrichtConn = new WebSocket('ws://localhost:8085/nachricht');
+nachrichtConn.onmessage = function(e) {
     console.log('Received', e.data);
     alert(e.data);
 };
